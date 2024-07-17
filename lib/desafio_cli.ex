33fe0220541a2a6135ac6fab6a44ac8ab9812a61
Mozart Maia <mozartmaia89@gmail.com>
2024-07-17 12:27:13 -0300

    feat: trying some stuff

--- lib/desafio_cli.ex
+++ lib/desafio_cli.ex
@@ -8,10 +8,39 @@ defmodule DesafioCli do
   comando como lista de strings e executa a CLI.
   """
   def main(_args) do
-    IO.puts(phrase())
+    # IO.puts(phrase())
+    result = IO.gets("Insira um nome por linha\n")
+    while(result)
+    # IO.puts(result)
+  end
+
+  def while(condicional) do
+    case condicional do
+      "\n" ->
+        :ok
+      x ->
+        IO.puts(x)
+        nextInput = IO.gets("")
+        while(nextInput)
+    end
+
   end
 
   def phrase() do
-    "Hello, world!"
+    args = System.argv()
+    if length(args) > 1 do
+      "Uso do script: desafio.cli <nomedoarquivo.txt>"
+    else
+      path = hd(args)
+      IO.puts(path)
+
+      case File.read(path) do
+        {:ok, result} -> IO.puts("Successo: \n#{result}")
+        {:error, reason} -> IO.puts("Erro ao ler o arquivo: #{reason}")
+      end
+
+      # IO.puts(result)
+      args
+    end
   end
 end
